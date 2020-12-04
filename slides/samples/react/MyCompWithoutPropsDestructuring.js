4311768187bb55e9976e36600a509e243138a905
Phani Rahul Sivalenka <rahul.sivalenka@corecompete.com>
2020-12-04 06:38:50 +0530

    Add information regarding props

---
+++ slides/samples/react/MyCompWithoutPropsDestructuring.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react';
+
+const MyComp = props => {
+  const { name, onNameChange } = props;
+
+  const [count, setCount] = useState(0);
+
+  // init
+  useEffect(
+    () => {
+      // Do something on
+      // initialization
+    },
+    [] /* This should be empty array 
+    for useEffect to act as init */
+  );
+
+  // update
+  useEffect(() => {
+    // Do something on name change
+  }, [name]);
+
+  // update on multiple
+  // state or prop changes
+  useEffect(() => {
+    // Do something on changing
+    // name or count
+  }, [name, count]);
+
+  const onChangeNameClick = () => {
+    const newName = getRandomName();
+    onNameChange(newName);
+  };
+
+  const increment = () => {
+    // Using the closure 'count' variable
+    setCount(count + 1);
+    // or by passing a callback
+    // which gets latest value of count 'c'
+    // setCount(c => c + 1);
+  };
+
+  return (
+    <div>
+      MyComp {name}
+      <button onClick={onChangeNameClick}>Change Name</button>
+      {count}
+    </div>
+  );
+};
+
+export default MyComp;
